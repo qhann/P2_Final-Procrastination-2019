f72f89b533589b484c42c22a91860f717d206910
Max <cybergore.dali@gmail.com>
2019-06-21 18:24:46 +0200

    no jQuery

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import room from "./roomwide.png";
 import "./App.css";
 import update from "immutability-helper";
-import jQuery from "jquery";
 
 import StatusBar from "./components/StatusBar";
 import Artwork from "./components/Artwork";
@@ -71,42 +70,6 @@ class App extends React.Component {
       })
     );
 
-    /*
-     * Replace all SVG images with inline SVG
-     */
-    jQuery("img.svg").each(function() {
-      var $img = jQuery(this);
-      var imgID = $img.attr("id");
-      var imgClass = $img.attr("class");
-      var imgURL = $img.attr("src");
-
-      jQuery.get(imgURL, function(data) {
-        // Get the SVG tag, ignore the rest
-        var $svg = jQuery(data).find("svg");
-
-        // Add replaced image's ID to the new SVG
-        if (typeof imgID !== "undefined") {
-          $svg = $svg.attr("id", imgID);
-        }
-        // Add replaced image's classes to the new SVG
-        if (typeof imgClass !== "undefined") {
-          $svg = $svg.attr("class", imgClass + " replaced-svg");
-        }
-
-        // Remove any invalid XML tags as per http://validator.w3.org
-        $svg = $svg.removeAttr("xmlns:a");
-
-        // Replace image with new SVG
-        $img.replaceWith($svg);
-
-        // Add an handler
-        jQuery("path").each(function() {
-          jQuery(this).click(function() {
-            alert(jQuery(this).attr("id"));
-          });
-        });
-      });
-    });
   }
 
   componentWillUnmount() {
