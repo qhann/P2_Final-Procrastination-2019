38116e678ef0fee6046ca0c804f68aecee6629c3
juliaxyy <julia.muenzer@stud.h-da.de>
2019-06-26 17:59:30 +0200

    anfang screens

--- src/App.js
+++ src/App.js
@@ -3,23 +3,53 @@ import "./App.css";
 import update from "immutability-helper";
 import Room from "./components/Room";
 import Intro from "./components/Intro";
+import Start from "./components/Start";
+import CharacterSelection from "./components/CharacterSelection";
 
 class App extends React.Component {
   state = {
-    current: "intro"
+    current: "start",
+    playerName: "",
+    gender: ""
+  };
+
+  handleStart() {
+    this.setState({ current: "characterSelection" });
   }
-  
+
+  handleIntroStart(gender, name) {
+    if (gender && name) {
+      this.setState({ current: "intro", playerName: name, gender: gender });
+    } else {
+      console.log("missing information");
+    }
+  }
 
   handleIntroEnd() {
-    this.setState({current: "room"})
+    this.setState({ current: "room" });
   }
 
   render() {
-
     return (
       <div className="App">
-          { this.state.current == "intro" ? <Intro endIntro={() => this.handleIntroEnd()}/> : null}
-          { this.state.current == "room" ? <Room /> : null}
+        {this.state.current == "start" ? (
+          <Start start={() => this.handleStart()} />
+        ) : null}
+        {this.state.current == "characterSelection" ? (
+          <CharacterSelection
+            startIntro={(gender, name) => this.handleIntroStart(gender, name)}
+          />
+        ) : null}
+        {this.state.current == "intro" ? (
+          <Intro
+            playerName={this.state.playerName}
+            endIntro={() => this.handleIntroEnd()}
+          />
+        ) : null}
+
+        {this.state.current == "room" ? (
+          <Room playerName={this.state.playerName} gender={this.state.gender} />
+        ) : null}
       </div>
     );
   }
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,13 +1,35 @@
 import React, { Component } from "react";
+import Mentor from "./mentor.svg";
 
 class Intro extends Component {
-
-
   render() {
-    const {endIntro} = this.props
+    const { endIntro, playerName } = this.props;
     return (
-      <div className={"intro"} onClick={endIntro}>
-        <p> INTRO </p>
+      <div className={"intro"}>
+        <button className={"button-room-start"} onClick={endIntro}>
+          Start
+        </button>
+        <p className={"intro-text"}>
+          Hallo {playerName}, <br />
+          stell dir vor dein großes Ziel ist es, eines Tages ein erfolgreicher
+          Künstler zu werden, der sich mit dem Schaffen von Skulpturen und
+          Installationen beschäftigt. Diese Leidenschaft ist in deiner Kindheit
+          entstanden und hat sich als einfaches Hobby zu deinem Traum
+          entwickelt. Du möchtest anderen Menschen helfen, den Weg zur Kunst zu
+          finden und sie genauso begeistern wie es Kunstwerke bei dir schaffen.
+          <br />
+          Momentan arbeitest du tagsüber in einem Büro und fühlst dich dort
+          nicht ausreichend kreativ ausgelastet. Nun möchtest du mit deiner
+          ersten Skulptur beginnen, die du daraufhin veröffentlichen kannst. Du
+          wirst ab jetzt drei Tage deine Freizeit zwischen 18 Uhr abends und 6
+          Uhr morgens erleben und erhältst die Chance deinen Traum zu erfüllen.
+          Während dem Arbeiten an der Skulptur ist es möglich, Fleißpunkte zu
+          sammeln, wenn du dich nicht zu sehr von anderen Faktoren ablenken
+          lässt. Du solltest immer deine Gesundheit im Blick behalten und
+          dementsprechend überlegte Entscheidungen treffen. Diese werden
+          Konsequenzen für deine Zukunft haben.
+        </p>
+        <img src={Mentor} className={"intro-mentor"} />
       </div>
     );
   }
