3ef921a6fdd689cc738cd04ad37c3f6feb0339af
Max <cybergore.dali@gmail.com>
2019-07-02 16:21:15 +0200

    Character Selection update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import EndScreen from "./components/EndScreen";
 
 class App extends React.Component {
   state = {
-    currentScreen: "room",
+    currentScreen: "start",
     playerName: "",
     gender: "girl"
   };
