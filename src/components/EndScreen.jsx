1630548dc50a0bdad98dd0808d235a874d8705a3
Max <cybergore.dali@gmail.com>
2019-07-06 14:43:10 +0200

    EndScreen update

--- src/components/EndScreen.jsx
+++ src/components/EndScreen.jsx
@@ -5,6 +5,7 @@ import endBoy_sickest from "./Player/end-boy-sickest.svg"
 import trophy1 from "./SVGs/trophy1.svg"
 import trophy2 from "./SVGs/trophy2.svg"
 import trophy3 from "./SVGs/trophy3.svg"
+import trophyStand from "./SVGs/trophyStand.svg"
 import Cat from "./Cat";
 
 
@@ -17,7 +18,7 @@ class EndScreen extends Component {
 
   render() {
     const { nextScreen, playerName, gender, artWork, sleepTime } = this.props;
-    let tiredness 
+    let tiredness
     if (sleepTime < 4 * 60 * 3) {
       tiredness = "sickest"
     } else if (sleepTime < 6 * 60 * 3) {
@@ -36,16 +37,16 @@ class EndScreen extends Component {
     workScore = (1 - percentExhaustion) * percentWorkTime * 100
     workScore = workScore ? ~~workScore : 0
 
-    awardsCount = 3
-    if (workScore > 40 ) {
+    awardsCount = 1
+    if (workScore > 40) {
       awardsCount = 1
-    } else if ( workScore > 40 && workScore < 80) {
+    } else if (workScore > 40 && workScore < 80) {
       awardsCount = 2
-    } else if ( workScore > 80) {
+    } else if (workScore > 80) {
       awardsCount = 3
     }
 
-  
+
     return (
       <div className={"end-screen"}>
         {/* Awards */}
@@ -54,15 +55,18 @@ class EndScreen extends Component {
         {/* CharHealth */}
         {/* Advice */}
         {/* <Player player={player} /> */}
-        <Cat className={"catFadeIn"} moving={false} player={{action: "none"}} direction={"left"} time={1} position={{x: 1200, y:700}} />
+        <Cat className={"catFadeIn"} moving={false} player={{ action: "none" }} direction={"left"} time={1} position={{ x: 1200, y: 700 }} />
         <img className={"end-art"} src={artWork} />
         {/* <img className={"end-player"} src={endBoy_sickest} alt=""/> */}
-        <Player tiredness={tiredness} location={"end"}/>
+        <Player tiredness={tiredness} location={"end"} />
         <MentorTip text={["Hier deine Endwerte. Bittesehr, ", playerName + ".", <br />, "mediumExhaustion: " + ~~this.props.workingExhaustion + " workTime: " + this.props.workTime + " workScore: " + workScore + " sleepTime: " + sleepTime]} />
-        <div className={"trophy-wrapper"}>
-          { awardsCount >= 1 ? <img className={"trophy trophy-min"} src={trophy1} alt=""/> : null}
-          { awardsCount >= 2 ? <img className={"trophy trophy-med"} src={trophy2} alt=""/> : null}
-          { awardsCount >= 3 ? <img className={"trophy trophy-max"} src={trophy3} alt=""/> : null}
+        <div className={"award-wrapper"}>
+          <img src={trophyStand} className={"trophy-stand"} alt="" />
+          <div className={"trophy-wrapper"}>
+            {awardsCount >= 1 ? <img className={"trophy trophy-min"} src={trophy1} alt="" /> : null}
+            {awardsCount >= 2 ? <img className={"trophy trophy-med"} src={trophy2} alt="" /> : null}
+            {awardsCount >= 3 ? <img className={"trophy trophy-max"} src={trophy3} alt="" /> : null}
+          </div>
         </div>
       </div>
     );
